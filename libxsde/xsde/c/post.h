efb44d24d5c9f70c28c758bca4683f2e249d7078
Boris Kolpackov <boris@codesynthesis.com>
2011-02-08 11:32:55 +0200

    Disable VC++ warning 4996 (deprecated POSIX) in C code

--- libxsde/xsde/c/post.h
+++ libxsde/xsde/c/post.h
@@ -4,6 +4,12 @@
  * license   : GNU GPL v2 + exceptions; see accompanying LICENSE file
  */
 
+#ifdef _MSC_VER
+#  if (_MSC_VER >= 1400)
+#    include <xsde/c/compilers/vc-8/post.h>
+#  endif
+#endif
+
 #if defined(__ghs__) || defined(__ghs) || defined(ghs)
 #  include <xsde/c/compilers/ghs/post.h>
 #endif
