8d1b88d3c4809fda041cec0ba3bc5c7e88c8f026
Boris Kolpackov <boris@codesynthesis.com>
2020-08-12 16:34:44 +0200

    Import additional Expat tweak

--- libxsde/xsde/c/expat/xmltok_impl.c
+++ libxsde/xsde/c/expat/xmltok_impl.c
@@ -1762,11 +1762,12 @@ PREFIX(updatePosition)(const ENCODING *enc,
 #define LEAD_CASE(n) \
     case BT_LEAD ## n: \
       ptr += n; \
+      pos->columnNumber++; \
       break;
     LEAD_CASE(2) LEAD_CASE(3) LEAD_CASE(4)
 #undef LEAD_CASE
     case BT_LF:
-      pos->columnNumber = (XML_Size)-1;
+      pos->columnNumber = 0;
       pos->lineNumber++;
       ptr += MINBPC(enc);
       break;
@@ -1775,13 +1776,13 @@ PREFIX(updatePosition)(const ENCODING *enc,
       ptr += MINBPC(enc);
       if (ptr < end && BYTE_TYPE(enc, ptr) == BT_LF)
         ptr += MINBPC(enc);
-      pos->columnNumber = (XML_Size)-1;
+      pos->columnNumber = 0;
       break;
     default:
       ptr += MINBPC(enc);
+      pos->columnNumber++;
       break;
     }
-    pos->columnNumber++;
   }
 }
 
